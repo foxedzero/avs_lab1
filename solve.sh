00126be38ec5bb4973726730dff78a228d56ca83
foxedzero <mregovskoy@gmail.com>
2026-09-23 13:02:39 +0300

    Part III

--- solve.sh
+++ solve.sh
@@ -1,8 +1,23 @@
-cp experiments/tasting_results victor/office/successful_experiment
-cp -r victor/kitchen/molecular_station experiments/molecular_backup
-ln -s ../victor/kitchen/molecular_station/foam_recipe experiments/current_recipe
-ln -s victor/kitchen kitchen_entry
-ln opening_message victor/kitchen/shift_order
-cat victor/kitchen/hot_station/senya_report victor/kitchen/hot_station/fedya_report > victor/kitchen/team_report
-cat victor/kitchen/chef_order >> experiments/tasting_results
-mv victor/hall/reservations victor/office/evening_reservations
+ls -lR | grep '^-' | sort -k5n | tail -5
+grep -hriE "катя|баринов" victor experiments | grep -iv 'гост' | sort -r | head -6
+grep -rli "катя" victor/kitchen/molecular_station experiments/molecular_backup | wc -l
+
+for i in victor/kitchen/hot_station/*_report; do
+    head -1 "$i"
+    tail -1 "$i"
+done | grep -iE 'блюд|баринов' | sort
+
+grep -viE 'Сеня|Федя' victor/kitchen/team_report | grep -i 'блюд' | sort -r | wc -w
+
+ls -lR | grep '^l' | sort -rk9
+
+ls -liR | grep '^-' | awk '$2 == 2' | sort -k1n
+
+rm experiments/tasting_results
+rm experiments/current_recipe
+rm kitchen_entry
+rm opening_message
+rm victor/kitchen/shift_order
+rm victor/kitchen/molecular_station/nitrogen_notes
+rmdir archive_empty
+rm -r experiments/molecular_backup
